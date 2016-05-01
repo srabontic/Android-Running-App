cf7471ca0521e27055be0198f20c641f6c4d6cb7
Srabonti <Srabonti>
2016-05-01 17:35:52 -0500

    MapsActivity Compete

--- app/src/main/java/com/example/user/demo_gps/MapsActivity.java
+++ app/src/main/java/com/example/user/demo_gps/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.demo_gps;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -9,11 +10,13 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -239,5 +242,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         return true;
     }
+
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        Toast.makeText(getApplicationContext(),
+                "YOU TOUCHED THE NAV DRAWER", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),
+                item.toString(), Toast.LENGTH_LONG).show();
+
+        if (id == R.id.Trip_Log) {
+            Toast.makeText(getApplicationContext(),
+                    "TRIP LOG", Toast.LENGTH_LONG).show();
+            Intent tlActivity= new Intent(this, TripLog.class);
+            startActivity(tlActivity);
+            return true;
+        } else if (id == R.id.Statistics) {
+            Toast.makeText(getApplicationContext(),
+                    "Statistics", Toast.LENGTH_LONG).show();
+            Intent sActivity= new Intent(this, Statistics.class);
+            startActivity(sActivity);
+            return true;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 }
