5c517cd1ecc723b09e748e695af0f39e06e4734d
Srabonti <Srabonti>
2016-05-01 22:13:59 -0500

    MapsActivity Compete

--- app/src/main/java/com/example/user/demo_gps/Trip.java
+++ app/src/main/java/com/example/user/demo_gps/Trip.java
@@ -3,23 +3,30 @@ package com.example.user.demo_gps;
 /**
  * Created by Rupesh on 4/26/2016.
  */
+//this is the trip class which is used to store and retrieve trip records from text file
 public class Trip {
     private String duration;
-    private double distance;
-    private float numSteps;
-    private float avgSpeed;
-    private float maxSpeed;
-    private float pace;
-    private String description;
+    private String distance;
+    private String numSteps;
+    private String avgSpeed;
+    private String dateOfTrip;
 
+    public String getDateOfTrip() {
+        return dateOfTrip;
+    }
+
+    public void setDateOfTrip(String dateOfTrip) {
+        this.dateOfTrip = dateOfTrip;
+    }
+
+    /********Created By: Srabonti Chakraborty**************/
     public Trip(){
         this.duration = "00:00:00";
-        this.distance = 0;
-        this.numSteps = 0;
-        this.avgSpeed = 0;
-        this.maxSpeed = 0;
-        this.pace = 0;
-        this.description = "";
+        this.distance = "0.000";
+        this.numSteps = "0";
+        this.avgSpeed = "0.000";
+        this.dateOfTrip = "00/00/0000";
+
     }
 
     public String getDuration() {
@@ -30,51 +37,39 @@ public class Trip {
         this.duration = duration;
     }
 
-    public double getDistance() {
+    public String getDistance() {
         return distance;
     }
 
-    public void setDistance(double distance) {
+    public void setDistance(String distance) {
         this.distance = distance;
     }
 
-    public float getNumSteps() {
+    public String getNumSteps() {
         return numSteps;
     }
 
-    public void setNumSteps(float numSteps) {
+    public void setNumSteps(String numSteps) {
         this.numSteps = numSteps;
     }
 
-    public float getAvgSpeed() {
+    public String getAvgSpeed() {
         return avgSpeed;
     }
 
-    public void setAvgSpeed(float avgSpeed) {
+    public void setAvgSpeed(String avgSpeed) {
         this.avgSpeed = avgSpeed;
     }
 
-    public float getMaxSpeed() {
-        return maxSpeed;
-    }
 
-    public void setMaxSpeed(float maxSpeed) {
-        this.maxSpeed = maxSpeed;
-    }
+    /********Created By: Srabonti Chakraborty**************/
 
-    public float getPace() {
-        return pace;
-    }
-
-    public void setPace(float pace) {
-        this.pace = pace;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public Trip(String duration, String distance, String numSteps,String avgSpeed, String dateOfTrip) {
+        super();
+        this.duration = duration;
+        this.distance = distance;
+        this.numSteps = numSteps;
+        this.avgSpeed = avgSpeed;
+        this.dateOfTrip = dateOfTrip;
     }
 }
